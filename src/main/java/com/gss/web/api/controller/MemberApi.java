8a6ef596ab966259d06e874bb76899bec9bc9db6
polrap <digh615@naver.com>
2022-07-14 13:20:06 +0900

    프로젝트 환경설정

--- src/main/java/com/gss/web/api/controller/MemberApi.java
+++ src/main/java/com/gss/web/api/controller/MemberApi.java
@@ -1,32 +1,42 @@
 package com.gss.web.api.controller;
 
 import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.gss.web.api.dto.MemberCreateDto.MemberCreateRequest;
-import com.gss.web.common.service.MemberService;
+import com.gss.web.common.domain.Member;
+import com.gss.web.common.service.MemberServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/singup")
 public class MemberApi {
 	private Logger logger = org.slf4j.LoggerFactory.getLogger(MemberApi.class);
 
-	private final MemberService memberService;
-
+	private final MemberServiceImpl memberServiceImpl;
+	
+	@GetMapping("/join")
+	public String joinPageTest(Model model) {
+		Member email= memberServiceImpl.findByEmail("test1@naver.com");
+		model.addAttribute("email", email);
+		return "join/singup";
+	}
+	
 	@PostMapping("/join")
 	public String joinMember(@RequestBody MemberCreateRequest dto) {
 		String path = "";
 		logger.info("[POST] :::: email : " + dto.getEmail());
-		if (memberService.checkEmail(dto.getEmail())) {
+		if (memberServiceImpl.checkEmail(dto.getEmail())) {
 			path = "/singup/join";
 		} else {
-			memberService.joinUp(dto);
+			memberServiceImpl.joinUp(dto);
 			path = "/login";
 		}
 		System.out.println("MemberApi::::::::"+path);
